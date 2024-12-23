ad046f858ee31beb3d53296801f48cc76b0f9133
Oruko-pj <114067904+Oruko-pj@users.noreply.github.com>
2024-12-23 08:47:41 +0000

    Adding Dependancies | Deno Essential Deno Command

--- main.ts
+++ main.ts
@@ -1,10 +1,5 @@
-import express from "npm:express@4.21.1";
-const app = express();
-
-app.get("/", (_req, res) => {
-        res.send("Hello, World!");
-});
+import { hello } from "https://gist.githubusercontent.com/uncomfyhalomacro/b5de275dfcbaeff1d2d89739541e4e6f/raw/648e323e4ab872cc2a6eeafdd1ccf46d8f6c6301/mod.ts";
 
 if (import.meta.main) {
-        app.listen({ port: 8080 });
+  hello();
 }
