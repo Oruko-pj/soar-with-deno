678e7e1e8996876f5366adb7e3873183f14cb5d6
Oruko-pj <114067904+Oruko-pj@users.noreply.github.com>
2024-12-23 09:16:28 +0000

    Upgrading and downgrading Deno

--- main.ts
+++ main.ts
@@ -1,5 +1,11 @@
-import { hello } from "https://gist.githubusercontent.com/uncomfyhalomacro/b5de275dfcbaeff1d2d89739541e4e6f/raw/648e323e4ab872cc2a6eeafdd1ccf46d8f6c6301/mod.ts";
-
 if (import.meta.main) {
-  hello();
+  Deno.serve((_req_) => {
+    (async () => {
+      for await (const el of Deno.readDir(".")) {
+        console.log(el.name);
+      }
+    })();
+    const hello = new Response("Hello, Stackie!");
+    return hello;
+  });
 }
